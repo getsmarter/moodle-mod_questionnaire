14f86794908a0031ac996819994f067e8d4179ee
Kurvin Hendricks <khendricks@C02XP0JUJG5L.local>
2019-01-30 14:12:11 +0200

    working on the hiding of the submit button, basic skeleton of parsing content to the js folder, using ionic disabled for now

---
+++ javascript/mobile_2.js
@@ -0,0 +1,17 @@
+setTimeout(function() { 
+    console.log("DOM is available now, yes yes it is"); 
+    
+    window.clicked_input = e => {
+        checkIfFinalRequiredResponse(e);
+    };
+
+}, 300);
+
+function checkIfFinalRequiredResponse (e) {
+
+    if(e[0] == e[1]) { //basic idea behind the validation for the button hiding logic, using disabled for now since it's an option in ionic
+        var button = document.getElementsByClassName("button button-md button-default button-default-md button-block button-block-md");
+        button.mod_questionnaire_submit_questionnaire_response.disabled = false;
+    }
+
+}
