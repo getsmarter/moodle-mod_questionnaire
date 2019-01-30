a7f02fbe98030e0bf41ed740e2414669400dbe55
Kurvin Hendricks <khendricks@C02XP0JUJG5L.local>
2019-01-30 14:18:07 +0200

    proof of concept, possible EOD commit

--- javascript/mobile_2.js
+++ javascript/mobile_2.js
@@ -1,5 +1,8 @@
 setTimeout(function() { 
     console.log("DOM is available now, yes yes it is"); 
+
+    // var button = document.getElementsByClassName("button button-md button-default button-default-md button-block button-block-md");
+    // button.mod_questionnaire_submit_questionnaire_response.hidden = false;
     
     window.clicked_input = e => {
         checkIfFinalRequiredResponse(e);
@@ -12,6 +15,8 @@ function checkIfFinalRequiredResponse (e) {
     if(e[0] == e[1]) { //basic idea behind the validation for the button hiding logic, using disabled for now since it's an option in ionic
         var button = document.getElementsByClassName("button button-md button-default button-default-md button-block button-block-md");
         button.mod_questionnaire_submit_questionnaire_response.disabled = false;
+        var button = document.getElementsByClassName("button button-md button-default button-default-md button-block button-block-md");
+        button.mod_questionnaire_submit_questionnaire_response.hidden = false;
     }
 
 }
