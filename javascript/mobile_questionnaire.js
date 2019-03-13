cef3b2a5bf22239d5f0ad5dfa31c3f3cc83d4e11
kurvinh <khendricks@2u.com>
2019-03-13 12:36:53 +0200

    once completed and na display as na

--- javascript/mobile_questionnaire.js
+++ javascript/mobile_questionnaire.js
@@ -3,6 +3,7 @@ setTimeout(function() {
     var allRangeCheck = document.getElementsByClassName('hidden-submit-button-check-false');
     var allSliders = document.getElementsByClassName('range range-md');
 
+
     if(typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined' && allRangeCheck.length == 0) { //basic idea behind the validation for the button hiding logic, using disabled for now since it's an option in ionic
         button.mod_questionnaire_submit_questionnaire_response.disabled = true;
     }
@@ -16,16 +17,23 @@ setTimeout(function() {
         checkboxes[i].childNodes[0].className+= ' ' + 'checkbox-checked';
     };
 
-    var targetNodes = document.getElementsByClassName('range range-md');
-    var observerOptions = {
-      childList: true,
-      attributes: true,
-      subtree: true, //Omit or set to false to observe only changes to the parent node.
-      characterData: true,
-    }
-
     var allNaApplicableSliders = document.getElementsByClassName('na-applicable');
     if(typeof(allNaApplicableSliders) != 'undefined' && allNaApplicableSliders.length > 0) {
+        var completedSliders = document.getElementsByClassName('na-applicable na-completed');
+
+        if(typeof(completedSliders) != 'undefined' && completedSliders.length > 0) {
+            for(var i = 0; i < completedSliders.length; i++) {
+                completedSliders[i].innerHTML = 'n/a';
+            }
+            return;
+        }
+
+        var observerOptions = {
+            childList: true,
+            attributes: true,
+            subtree: true, //Omit or set to false to observe only changes to the parent node.
+            characterData: true,
+        }
         for(var x = 0; x < allNaApplicableSliders.length; x++) {
             var observer = new MutationObserver(callback);
             observer.observe(allNaApplicableSliders[x], observerOptions);
