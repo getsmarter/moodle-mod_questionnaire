add0dd51e76c84857cfb898fcfd705f61e364343
kurvinh <khendricks@2u.com>
2019-03-18 09:09:06 +0200

    working on branching required scenarios

--- javascript/mobile_questionnaire.js
+++ javascript/mobile_questionnaire.js
@@ -1,11 +1,16 @@
+var requireSliderInputs = [];
+
 setTimeout(function() {
 
     var button = document.getElementsByClassName('button button-md button-default button-default-md button-block button-block-md');
     var allRangeCheck = document.getElementsByClassName('hidden-submit-button-check-false');
     var allSliders = document.getElementsByClassName('range range-md');
+    var nextButton = document.getElementsByClassName('next-button button button-md button-outline button-outline-md button-block button-block-md');
 
-    if(typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined' && !allRangeCheck) { //basic idea behind the validation for the button hiding logic, using disabled for now since it's an option in ionic
+    if(typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined') { //basic idea behind the validation for the button hiding logic, using disabled for now since it's an option in ionic
         button.mod_questionnaire_submit_questionnaire_response.disabled = true;
+    } else {
+        nextButton.mod_questionnaire_submit_questionnaire_branching.disabled = true;
     }
 
     var requiredInputs = []; //required inputs, this is an array with references to the required inputs for the questionnaire
@@ -45,6 +50,7 @@ setTimeout(function() {
     }
 
     if(typeof(allSliders) != 'undefined' && allSliders.length > 0) { //NA onload 
+        console.log('oooohohohhoh');
         for(var x = 0; x < allSliders.length; x++) {
             var counter = 1;
             //here I need to check if it is na applicable
@@ -61,6 +67,17 @@ setTimeout(function() {
                 allSliders[x].childNodes[1].children[i].style.width = '0px';
                 counter++;
             }
+        }
+
+        var observerOptions = {
+            childList: true,
+            attributes: true,
+            subtree: true, //Omit or set to false to observe only changes to the parent node.
+            characterData: true,
+        }
+        for(var x = 0; x < allSliders.length; x++) {
+            var observer = new MutationObserver(sliderObserver);
+            observer.observe(allSliders[x], observerOptions);
         }
     }
 }, 300);
@@ -86,8 +103,11 @@ function checkIfFinalRequiredResponse (e, requiredInputs) {
         requiredInput = true;
     }
     var button = document.getElementsByClassName('button button-md button-default button-default-md button-block button-block-md');
+    var nextButton = document.getElementsByClassName('next-button button button-md button-outline button-outline-md button-block button-block-md');
     if(requiredInput === true && numberOfRequiredAnswers ==  finalRequiredAnswer && typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
         button.mod_questionnaire_submit_questionnaire_response.disabled = false;
+    } else if(requiredInput === true && numberOfRequiredAnswers == finalRequiredInput && typeof(nextButton.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
+        nextButton.mod_questionnaire_submit_questionnaire_branching.disabled = false;
     }
 }
 
@@ -100,6 +120,86 @@ function callback(mutationList, observer) {
         if(currentNaValue == restingNaValue) {
             mutation.target.data = 'n/a';
         }
+
+        // if(!requireSliderInputs.includes(currentRequiredValue)) {
+        //     requireSliderInputs.push(currentRequiredValue); //only push if it has not been added to the array already
+        // }
+
+        // var numberOfRequiredAnswers = 0;
+        // for(var x = 0; x < requireSliderInputs.length; x++) {
+        //    //first need to check that all answers before required answer are in array
+        //    //then set a flag that I can check later
+        //     if(requireSliderInputs[x] <= finalRequiredInput) { //checking if the inputs are less than the required input or the required input
+        //         //and increment initial check
+        //         numberOfRequiredAnswers++;
+        //     } 
+        // }
+
+        // var requiredInput = false;
+        // if(requireSliderInputs.includes(finalRequiredInput)) {
+        //     requiredInput = true;
+        // }
+        // var button = document.getElementsByClassName('button button-md button-default button-default-md button-block button-block-md');
+        // var nextButton = document.getElementsByClassName('next-button button button-md button-outline button-outline-md button-block button-block-md');
+        // if(requiredInput === true && numberOfRequiredAnswers == finalRequiredInput && typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
+        //     button.mod_questionnaire_submit_questionnaire_response.disabled = false;
+
+        // } else if(requiredInput === true && numberOfRequiredAnswers == finalRequiredInput && typeof(nextButton.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
+        //     nextButton.mod_questionnaire_submit_questionnaire_branching.disabled = false;
+        // }
+
+        break;
+    }
+  });
+}
+
+function sliderObserver(mutationList, observer) {
+  mutationList.forEach((mutation) => {
+    console.log(mutation.type);
+    switch(mutation.type) {
+        case 'characterData':
+        console.log('sliderObserver');
+        console.log(mutation.target.parentElement.parentElement.parentElement.parentElement);
+        // var restingNaValue = mutation.target.parentElement.getAttribute('data-final');
+        // var currentNaValue = mutation.target.data;
+        // var currentRequiredValue = mutation.target.parentElement.getAttribute('data-currentinput');
+        // var finalRequiredInput = mutation.target.parentElement.getAttribute('data-finalinput');
+        // if(currentNaValue == restingNaValue) {
+        //     mutation.target.data = 'n/a';
+        // }
+
+        // console.log(mutation);
+
+        // console.log(currentRequiredValue);
+        // console.log(finalRequiredInput);
+
+        // if(!requireSliderInputs.includes(currentRequiredValue)) {
+        //     requireSliderInputs.push(currentRequiredValue); //only push if it has not been added to the array already
+        // }
+
+        // var numberOfRequiredAnswers = 0;
+        // for(var x = 0; x < requireSliderInputs.length; x++) {
+        //    //first need to check that all answers before required answer are in array
+        //    //then set a flag that I can check later
+        //     if(requireSliderInputs[x] <= finalRequiredInput) { //checking if the inputs are less than the required input or the required input
+        //         //and increment initial check
+        //         numberOfRequiredAnswers++;
+        //     } 
+        // }
+
+        // var requiredInput = false;
+        // if(requireSliderInputs.includes(finalRequiredInput)) {
+        //     requiredInput = true;
+        // }
+        // var button = document.getElementsByClassName('button button-md button-default button-default-md button-block button-block-md');
+        // var nextButton = document.getElementsByClassName('next-button button button-md button-outline button-outline-md button-block button-block-md');
+        // if(requiredInput === true && numberOfRequiredAnswers == finalRequiredInput && typeof(button.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
+        //     button.mod_questionnaire_submit_questionnaire_response.disabled = false;
+
+        // } else if(requiredInput === true && numberOfRequiredAnswers == finalRequiredInput && typeof(nextButton.mod_questionnaire_submit_questionnaire_response) != 'undefined') {
+        //     nextButton.mod_questionnaire_submit_questionnaire_branching.disabled = false;
+        // }
+
         break;
     }
   });
