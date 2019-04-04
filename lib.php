1d758e52e42650196d9922e2cf72223936603974
kurvinh <khendricks@2u.com>
2019-04-04 15:08:07 +0200

    fixing multiple checkboxes being recorded only recording true now

--- lib.php
+++ lib.php
@@ -1028,7 +1028,7 @@ function save_questionnaire_data_branching($questionnaireid, $surveyid, $userid,
                         unset($missingquestions[$rquestionid]);
                         if ($typeid == $questionnairedata['questionsinfo'][$sec][$rquestionid]['type_id']) {
                             if ($rquestionid > 0 && !in_array($response['value'], array(-9999, 'undefined'))) {
-                                if( $typeid == QUESCHECK) { //if checkbox handle differently because we need to check if question value is set to true
+                                if( $typeid == QUESCHECK && $response['value'] == 'true') { //if checkbox handle differently because we need to check if question value is set to true
                                     if (isset($args[3]) && !empty($args[3])) {
                                         $choiceid = intval($args[3]);
                                         $rec = new \stdClass();
