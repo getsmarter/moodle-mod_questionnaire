b3b5f9eccb67c23fdc6f94f20f1b1c5618c13b25
Brett Fritz <bfritz@2u.com>
2023-07-28 16:06:13 +0200

    CTED-3085 | Enhancement | Accessibility 2023 | Styling

--- javascript/completepage.js
+++ javascript/completepage.js
@@ -1,10 +1,15 @@
 // CTED-3085 | Enhancement | Accessibility 2023 | Styling
 // Allow users to use the enter key to select radio buttons.
+// But also allow enter to work when a button is focused (e.g. submit button).
 document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('phpesp_response');
-    form.addEventListener('keypress', function(e) {
+    var form = document.getElementById("phpesp_response");
+    document.addEventListener("keydown", function(e) {
         if (e.keyCode === 13) {
-            e.preventDefault();
+            if (e.target.type === "submit") {
+                form.submit();
+            } else {
+                e.preventDefault();
+            }
         }
     });
 }, false);
