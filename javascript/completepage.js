052c28c2616a566eb712ace5b3201d723c35d020
Brett Fritz <bfritz@2u.com>
2023-08-04 08:48:41 +0200

    CTED-3085 | Enhancement | Accessibility 2023 | Styling

--- javascript/completepage.js
+++ javascript/completepage.js
@@ -3,15 +3,15 @@
 // But also allow enter key to work when a button (or link) is focused (e.g. submit button).
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.getElementById("phpesp_response");
-    document.addEventListener("keydown", function(e) {
+    document.addEventListener("keydown", function (e) {
         if (e.keyCode === 13) {
             if (e.target.type === "submit") {
                 form.submit();
-           } else if (e.srcElement.contains("<a ")) {
+            } else if (e.srcElement.contains("<a ")) {
                 // Allow link clicks to also be actionable by enter key.
-            } else {
-                e.preventDefault();
+                return;
             }
+            e.preventDefault();
         }
     });
 }, false);
